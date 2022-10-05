bbd7bc35cb9e2fbaecaf304dd1448d2225dbd69c
Michael Bezzaponnyy <michael.bezzaponnyy@gmail.com>
2022-10-05 17:57:55 +0300

    Подготовит верстку для swiper

--- source/js/map.js
+++ source/js/map.js
@@ -1,14 +1,7 @@
-const COORDINATE = {
-  lat: 59.96831,
-  lng: 30.31748
-};
-
 const LAYER_OPTION = {
   url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 };
-
-const MAP_ZOOM = 17;
 
 const MARKER_OPTION = {
   iconUrl: './img/map/map-pin.svg',
@@ -16,20 +9,19 @@ const MARKER_OPTION = {
   iconAnchor: [19, 50]
 };
 
+const COORDINATE = {
+  lat: 59.96831,
+  lng: 30.31748
+};
+
+const MAP_ZOOM = 17;
+
 const mapLeaflet = document.querySelector('.map__leaflet');
 const mapImage = document.querySelector('.map__image');
 
 const map = L.map(mapLeaflet);
-const markerIcon = L.icon(MARKER_OPTION);
 
-const createCustomPopup = () => /*html*/`
-  <div class="popup">
-    <img class="popup__logo" src="img/logo/drink-to-go-large.svg" width="170" height="36" alt="Логотип «Drink2Go».">
-    <p class="popup__text">Ждем Вас ежедневно с 07:00 до 00:00</p>
-  </div>
-`;
-
-const buildMap = () => {
+export const buildMap = () => {
   map
     .on('load', () => {
       mapImage.replaceWith(mapLeaflet);
@@ -40,14 +32,22 @@ const buildMap = () => {
     LAYER_OPTION.url,
     {attribution: LAYER_OPTION.attribution}
   ).addTo(map);
+}
 
-  L.marker(
+const markerIcon = L.icon(MARKER_OPTION);
+const marker = L.marker(
   COORDINATE,
   {
     draggable: false,
     icon: markerIcon
   }
-  ).addTo(map).bindPopup(createCustomPopup());;
-}
+).addTo(map);
 
-export {buildMap};
+const createCustomPopup = () => /*html*/`
+  <div class="popup">
+    <img class="popup__logo" src="img/logo/drink-to-go-large.svg" width="170" height="36" alt="Логотип «Drink2Go».">
+    <p class="popup__text">Ждем Вас ежедневно с 07:00 до 00:00</p>
+  </div>
+`;
+
+marker.bindPopup(createCustomPopup());
